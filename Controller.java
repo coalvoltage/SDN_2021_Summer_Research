4e84895d158a42eb29466dad2aee8747f50c2ad2
coalvoltage <coalvoltage@gmail.com>
2021-06-15 17:06:53 -0700

    Create Controller.java
    Imported skeleton code from Learning Switch

---
+++ Controller.java
@@ -0,0 +1,78 @@
+package org.onosproject.learningswitch;
+
+import com.google.common.collect.Maps;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.onlab.packet.Ethernet;
+import org.onlab.packet.MacAddress;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.CoreService;
+import org.onosproject.net.ConnectPoint;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.flow.DefaultFlowRule;
+import org.onosproject.net.flow.DefaultTrafficSelector;
+import org.onosproject.net.flow.DefaultTrafficTreatment;
+import org.onosproject.net.flow.FlowRule;
+import org.onosproject.net.flow.FlowRuleService;
+import org.onosproject.net.packet.PacketContext;
+import org.onosproject.net.packet.PacketPriority;
+import org.onosproject.net.packet.PacketProcessor;
+import org.onosproject.net.packet.PacketService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Optional;
+
+
+@Component(immediate = true, enabled = false)
+public class LearningSwitchSolution {
+
+    // Instantiates the relevant services.
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected PacketService packetService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected FlowRuleService flowRuleService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected CoreService coreService;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    protected Map<DeviceId, Map<MacAddress, PortNumber>> macTables = Maps.newConcurrentMap();
+    private ApplicationId appId;
+    private PacketProcessor processor;
+
+    @Activate
+    protected void activate() {
+        log.info("Started");
+        appId = coreService.getAppId("<config name in xml>"); //equal to the name shown in pom.xml file
+
+        processor = new SwitchPacketProcessor();
+        packetService.addProcessor(processor, PacketProcessor.director(3));
+
+
+        packetService.requestPackets(DefaultTrafficSelector.builder()
+                .matchEthType(Ethernet.TYPE_IPV4).build(), PacketPriority.REACTIVE, appId, Optional.empty());
+        packetService.requestPackets(DefaultTrafficSelector.builder()
+                .matchEthType(Ethernet.TYPE_ARP).build(), PacketPriority.REACTIVE, appId, Optional.empty());
+    }
+
+
+    @Deactivate
+    protected void deactivate() {
+        log.info("Stopped");
+        packetService.removeProcessor(processor);
+    }
+
+
+    private class CustomPacketProcessor implements PacketProcessor {
+        @Override
+        //Implement here
+    }
+}

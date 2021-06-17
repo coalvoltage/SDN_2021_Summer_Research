4e41c98a96a9c79289b118525210e2196ea4e335
coalvoltage <coalvoltage@gmail.com>
2021-06-16 18:24:15 -0700

    Implementing Forwarding with IP

--- Controller.java
+++ Controller.java
@@ -7,7 +7,10 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.onlab.packet.Ethernet;
-import org.onlab.packet.MacAddress;
+import org.onlab.packet.IP;
+import org.onlab.packet.IPv4;
+import org.onlab.packet.IPv6;
+
 import org.onosproject.core.ApplicationId;
 import org.onosproject.core.CoreService;
 import org.onosproject.net.ConnectPoint;
@@ -25,6 +28,8 @@ import org.onosproject.net.packet.PacketContext;
 import org.onosproject.net.packet.PacketPriority;
 import org.onosproject.net.packet.PacketProcessor;
 import org.onosproject.net.packet.PacketService;
+import org.onosproject.net.host.HostService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,17 +48,22 @@ public class LearningSwitchSolution {
     protected FlowRuleService flowRuleService;
     
     @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected CoreService hostService;
+    
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
     protected TopologyService topologyService;
     
     @Reference(cardinality = ReferenceCardinality.MANDATORY)
     protected CoreService coreService;
 
+
+
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    protected Map<DeviceId, Map<MacAddress, PortNumber>> macTables = Maps.newConcurrentMap();
     private ApplicationId appId;
     private PacketProcessor processor;
     private TopologyListener topologyListener;
+    protected Map<IpElementId, DeviceId) ip2deviceTable = Maps.newConcurrentMap();
 
     @Activate
     protected void activate() {
@@ -64,13 +74,12 @@ public class LearningSwitchSolution {
         topologyListener = new CustomTopologyListener();
         
         packetService.addProcessor(processor, PacketProcessor.director(3));
-        
         topologyService.addListener(topologyListener);
 
         packetService.requestPackets(DefaultTrafficSelector.builder()
                 .matchEthType(Ethernet.TYPE_IPV4).build(), PacketPriority.REACTIVE, appId, Optional.empty());
-        packetService.requestPackets(DefaultTrafficSelector.builder()
-                .matchEthType(Ethernet.TYPE_IPV6).build(), PacketPriority.REACTIVE, appId, Optional.empty());
+        //packetService.requestPackets(DefaultTrafficSelector.builder()
+        //        .matchEthType(Ethernet.TYPE_IPV6).build(), PacketPriority.REACTIVE, appId, Optional.empty());
         packetService.requestPackets(DefaultTrafficSelector.builder()
                 .matchEthType(Ethernet.TYPE_ARP).build(), PacketPriority.REACTIVE, appId, Optional.empty());
     }
@@ -87,8 +96,52 @@ public class LearningSwitchSolution {
     private class CustomPacketProcessor implements PacketProcessor {
         @Override
         public void process(PacketContext pc) {
+            Short type = pc.inPacket().parsed().getEtherType();
+            
+            if(type != Ethernet.TYPE_IPV4 && type != Ethernet.TYPE_ARP) {
+                return;
+            }
+            
+            
+            IpAddress sourceIP = null;
+            IpAddress destinationIP = null;
+            
+            IPacket pkt = pc.inPacket().parsed().getPayload();
+            
+            if(type == Ethernet.TYPE_IPV4) {
+                IPv4 pktIpv4 = (IPv4) pkt;
+                sourceIP = IpAddress.valueOf(pktIpv4.getSourceAddress());
+                destinationIP =  IpAddress.valueOf(pktIpv4.getDestinationAddress());
+            }
+            else if(type == Ethernet.TYPE_ARP) {
+                ARP pktArp = (ARP) pkt;
+                sourceIP =  IpAddress.valueOf(IpAddress.Version.INET, pktIpv4.getSenderProtocolAddress());
+                destinationIP =  IpAddress.valueOf(IpAddress.Version.INET, pktIpv4.getTargetProtocolAddress());
+            }
+            
             //find what IPs is associated with which edge switch
+            Set<Host> sourceHosts = hostService.getHostsByIp(sourceIP);
+            Set<Host> destinationHosts = hostService.getHostsByIp(destinationIP);
+            
+            for(int i = 0; i < sourceHosts.size(); i++) {
+                log.info(sourceHosts[i].id().toString());
+            }
+            for(int i = 0; i < destinationHosts.size(); i++) {
+                log.info(destinationHosts[i].id().toString());
+            }
+            
             //get path associated with these edge
+            
+            Set<Path> foundPaths = topologyService.getDistjointPaths(topologyService.currentTopology(), sourceHosts[0].location().deviceId(), destinationHosts[0].location().deviceId());
+            
+            List<Links> currentPath = foundPaths[0].links();
+            Link destinationSwitch = null;
+            
+            for (int i = 0; i < currentPath.size(); i++) {
+                if(currentPath[i].src().deviceId() == pc.inPacket().receivedFrom().deviceId()) {
+                    destinationSwitch = currentPath[i];
+                }
+            }
             //install appropriate flow rules
             //timeout 60 secs?
         }
